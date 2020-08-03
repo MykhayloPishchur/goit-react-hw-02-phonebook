e338959f4b669cdda01f2915ec4030b0c231c286
Michael <mykhaylo88@icloud.com>
2020-08-03 10:54:31 +0300

    Updates

---
+++ precache-manifest.9909cbdb0e6b0f34dc8882aebe4657da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19528259cd5b75a85f9c066c4964bd4f",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "560baddd6b9ff52f2fb1",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.ef5d1af4.chunk.css"
+  },
+  {
+    "revision": "ff68479941b805af18dd",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.ffea6125.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.ffea6125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "560baddd6b9ff52f2fb1",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.25804d71.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
